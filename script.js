d7cf82c2e36e56047b01990b9ca87ba53dac63f9
Saturnfive <bevane@live.com>
2024-07-06 06:20:43 +0700

    feat: show and close book add form on button clicks

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const bookDisplay = document.querySelector('.book-display');
+const addBookButton = document.querySelector('.add-book');
+const addBookModal = document.querySelector('.add-book-modal');
+const bookModalClose = document.querySelector('.close');
 const myLibrary = [];
+
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -8,10 +12,12 @@ function Book(title, author, pages, read) {
     this.read = read;
 };
 
+
 function addBookToLibrary(title, author, pages, read) {
     const book = new Book(title, author, pages, read);
     return myLibrary.push(book);
 };
+
 
 function displayLibraryBooks(books) {
     for (const book in books) {
@@ -38,5 +44,13 @@ function displayLibraryBooks(books) {
 
         bookDisplay.appendChild(newBook);
     }
-
 };
+
+
+addBookButton.addEventListener("click", () => {
+    return addBookModal.show();
+});
+
+bookModalClose.addEventListener("click", () => {
+    return addBookModal.close();
+});
