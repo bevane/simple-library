7f5be3b8290f64520f51f19ab4653f011c892577
Saturnfive <bevane@live.com>
2024-07-05 13:34:59 +0700

    feat: add addbook and displaybook funcs

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
-console.log("hi")
+const bookDisplay = document.querySelector('.book-display');
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+};
+
+function addBookToLibrary(title, author, pages, read) {
+    const book = new Book(title, author, pages, read);
+    return myLibrary.push(book);
+};
+
+function displayLibraryBooks(books) {
+    for (const book in books) {
+        const newBook = document.createElement("div");
+        newBook.className = "book";
+
+        const deleteButton = document.createElement("button");
+        deleteButton.className = "delete";
+        deleteButton.innerText = "x";
+
+        const bookTitle = document.createElement("h4");
+        const by = document.createElement("p");
+        by.className = "by";
+        by.innerText = "by";
+        const bookAuthor = document.createElement("p");
+
+        bookAuthor.innerText = book.author;
+        bookTitle.innerText = book.title;
+
+        newBook.appendChild(deleteButton);
+        newBook.appendChild(bookTitle);
+        newBook.appendChild(by);
+        newBook.appendChild(bookAuthor);
+
+        bookDisplay.appendChild(newBook);
+    }
+
+};
